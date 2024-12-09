56cf309b5765df5a975121b5a7375184244c1393
imamhossain-bd <imamhossain0098@gmail.com>
2024-12-09 08:11:47 +0600

    header page update

---
+++ Includs/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/d69002f9fe.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <section>
+        <div>
+            <nav class="bg-slate-400 h-20 w-full">
+                <div class="px-40 flex justify-between items-center gap-5 pt-5">
+                    <div class="w-28">
+                        <img src="https://i.ibb.co.com/5Y53PdM/shohoz-logo-new.png" alt="">
+                    </div>
+                    <div>
+                        <ul class="flex gap-5">
+                            <li class="px-4 py-2 text-base font-semibold border-2 border-green-700 hover:border-green-700 hover:text-[#01652c] rounded-xl"><a href="#"><i class="fa-solid fa-bus"></i> BUS</a></li>
+                            <li class="px-4 py-2 text-base font-semibold hover:border-2 hover:border-green-700 hover:text-[#01652c] rounded-xl"><a href="#"><i class="fa-solid fa-jet-fighter-up"></i> Air</a></li>
+                            <li class="px-4 py-2 text-base font-semibold hover:border-2 hover:border-green-700 hover:text-[#01652c] rounded-xl"><a href="#"><i class="fa-solid fa-train-subway"></i> Train</a></li>
+                            <li class="px-4 py-2 text-base font-semibold hover:border-2 hover:border-green-700 hover:text-[#01652c] rounded-xl"><a href="#"><i class="fa-solid fa-ship"></i> Launch</a></li>
+                            <li class="px-4 py-2 text-base font-semibold hover:border-2 hover:border-green-700 hover:text-[#01652c] rounded-xl"><a href="#"><i class="fa-regular fa-calendar"></i> Event</a></li>
+                            <li class="px-4 py-2 text-base font-semibold hover:border-2 hover:border-green-700 hover:text-[#01652c] rounded-xl"><a href="#"><i class="fa-brands fa-slack"></i> Park</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </section>
+</body>
+</html>
--- main.php
+++ main.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="light">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <?php require_once("Includs/header.php")?>    
+
+
 </body>
 </html>
